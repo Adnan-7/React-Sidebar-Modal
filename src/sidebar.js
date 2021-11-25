9ad395e5e2e2197ad10995f46e0c2871792c6484
Adnan Khan <adnan7.zain@gmail.com>
2021-11-25 22:30:10 +0500

    create sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import logo from './logo.svg';
+import { links, social } from './data';
 
 const Sidebar = () => {
-  return <div>sidebar comp</div>;
+  return (
+    <aside className='sidebar show-sidebar'>
+      <div className='sidebar-header'>
+        <img src={logo} className='logo' alt='Adnan Khan' />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className='social-icons'>
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
